756bccc75be5aeeae3ba9bb2259d0d3e94b50a5c
qalintana <alintana18@gmail.com>
2020-06-06 01:11:50 +0100

    criacao do primeiro usuario terminado com sucesso

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+import "./database";
 import homeRoutes from "./routes/home.routes";
 
 class App {
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,6 +1,15 @@
+import Aluno from "../models/Aluno";
+
 class HomeController {
-  index(req, res) {
-    return res.json({ tudocerto: "tudo caminhando bem" });
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: "Queiroz",
+      sobrenome: "Santana",
+      email: "alintana22@gmail.com",
+      idade: 24,
+      peso: 1.92,
+    });
+    return res.json({ novoAluno });
   }
 }
 
