eee108842adaaf5f7fd585623a6d38ad8178c077
qalintana <alintana18@gmail.com>
2020-06-09 23:21:58 +0100

    adicionado o cors e helmete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import { resolve } from "path";
+import cors from 'cors';
+import helmet from 'helmet';
+
+
 dotenv.config();
 
 import "./database";
@@ -10,6 +14,21 @@ import tokenRoutes from "./routes/token.routes";
 import alunoRoutes from "./routes/aluno.routes";
 import fotoRoutes from "./routes/foto.routes";
 
+
+const whiteList = [
+  'https://locahost:3000'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('not Allowed'));
+    }
+  }
+};
 class App {
   constructor() {
     this.app = express();
@@ -18,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cor(corsOptions))
+    this.app.use(helmet())
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, "..", "uploads")));
